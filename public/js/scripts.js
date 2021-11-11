7fe64ad2500f9c6bc4c4a22abf2f3bf56418c4af
Diba <72705126+DibaKarimi@users.noreply.github.com>
2021-11-11 00:13:43 +0000

    remove js folder

--- public/js/scripts.js
+++
@@ -1,6 +0,0 @@
-const paginationResults = require("js/paginationResults");
-//import { pagination } from "js/paginationResults";
-function nextPage(data,page, searchKey) {
-  //const result = paginationResults(page,latestData);
-  console.log(data, page, searchKey, latestSearch);
-}
